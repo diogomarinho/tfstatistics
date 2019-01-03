2c8d05945409337a82a2c997ee8d24a37198a68e
diogoma <diogoma@bmb.sdu.dk>
2019-01-03 14:05:13 -0300

    implementations of classes for least square

--- leastsquares.py
+++ leastsquares.py
@@ -3,9 +3,14 @@ import numpy as np
 class leastSquaresSolver:
     def solve(self, X, Y):
         print('Solving least squares')
-
+#
 class byInverse(leastSquaresSolver):
     def solve(self, X, Y):
-        print('testing abstraction')
-        #inverse = np.dot(np.transpose(self.X), self.X)
-        #return(np.dot(inverse, (np.dot(np.transpose(self.X), self.Y))))
+        Xprod = np.dot(X.T, X)
+        Yprod = np.dot(X.T, Y)
+        return np.dot(np.linalg.inv(Xprod), Yprod)
+#
+class byGradientDescent(leastSquaresSolver):
+    def solve(self, X, Y):
+        # todo
+        pass;
--- lm.py
+++ lm.py
@@ -7,23 +7,26 @@ from leastsquares import leastSquaresSolver, byInverse
 # response variabl (Y): SalePrice
 # predictors: LotArea (Numerical) LotFrontage (Numerical) Street(Categorical)
 # example data to develop the linear regression method
+#
 def getXYData(fin='~/tfstatistics/house_pricing_train.csv'):
     df = pd.read_csv(fin)
-    xydata = df['SalePrice LotArea LotFrontage Street'.split()]
+    xydata = df['SalePrice LotArea'.split()]
     xydata = xydata.dropna(axis=0)
-    xydata.Street = xydata.Street.factorize()[0]
+    #xydata.Street = xydata.Street.factorize()[0]
+    # xydata.Street.cat.codes.values
     return xydata
-
+#
 class lm:
     def __init__(self, data, response_id):
         self.y = data[[response_id]].values
         self.x = data.drop([response_id], axis=1).values
-        # adding ones for the intercept
+        self.x = np.column_stack((self.x, np.ones(shape=(self.x.shape[0]))))
+    # .
     def fit(self, lss):
-        lss.solve(self.x, self.y)
-        return 0
-
+        return lss.solve(self.x, self.y)
+#
+# example
 df = getXYData()
 lreg = lm(df, 'SalePrice')
-lreg.fit(byInverse())
+print(lreg.fit(byInverse()))
 
